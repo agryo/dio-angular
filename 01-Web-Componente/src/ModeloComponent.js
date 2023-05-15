1935b6bed102113bbe278bb8b14d8b39543b2269
Agryo Araujo <agryostallion@gmail.com>
2023-05-14 22:31:58 -0300

    Iniciando

---
+++ 01-Web-Componente/src/ModeloComponent.js
@@ -0,0 +1,39 @@
+/* Aqui é um modelo de componente usado no HTML como uma Tag HTML.
+   Nesse caso, a própria Tag é criada aqui, uma Tag Customizada, Personalizada. 
+   A Tag funciona mais ou menos como um Objeto, na qual criamos um modelo a ser
+   usado varias vezes onde quiser com esse "molde" criado aqui. */
+
+/* De início é criado uma class e extends o HTMLElement, pois aqui será um Element do HTML. */
+class MeuComponent extends HTMLElement {
+    constructor() {
+        /* O constructor já chama o super() para "puxar" o HTMLElement. */
+        super();
+        /* Em seguida cria a variável shadow (Shadow D.O.M.) que "attacha", cria o shadow e
+           passa como parâmero um Objeto (mode: open) que indica que a shadow vai ser aberta,
+           aceita ser modificada por fora, outro JS.
+           (Poder ser fechada (mode: closed) que não aceita ser modificada por fora.) */
+        const shadow = this.attachShadow({ mode: "open" });
+    
+        /* Aqui é a base do componente, o que irá entrar no meu HTML na página. */
+        const componentRoot = document.createElement("h1");
+        componentRoot.textContent = "Felipe";
+    
+        /* Aqui é o estilo (CSS), que vai ser modificado apenas no componente que estou usando.
+           Ele já vai no formato exato de como ele foi programado, estilizado e entra em qualquer
+           parte do HTML sem bagunçar o estilo do próprio site. Diminuindo assim o CSS do site. */
+        const style = document.createElement("style");
+        style.textContent = `
+            h1{
+            color: red
+            }
+        `;
+    
+        
+        shadow.appendChild(componentRoot);
+        shadow.appendChild(style);
+    }
+  }
+  /* E aqui ele define, envia o componente para ser usado no HTML como Tag. 
+     Especificando o nome da Tag (sempre com um "-" (hifen) para diferenciar das Tags originais) 
+     caso contrário vai dar problema. */
+  customElements.define("meu-componente", MeuComponent);

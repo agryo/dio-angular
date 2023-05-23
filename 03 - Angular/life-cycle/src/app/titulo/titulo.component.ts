0a19f8a74b4616c45aa2b883e9ed0c58be642ace
Agryo Araujo <agryostallion@gmail.com>
2023-05-22 22:34:07 -0300

    Vida útel do componente.

--- 03 - Angular/life-cycle/src/app/titulo/titulo.component.ts
+++ 03 - Angular/life-cycle/src/app/titulo/titulo.component.ts
@@ -1,17 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-titulo',
   templateUrl: './titulo.component.html',
   styleUrls: ['./titulo.component.css']
 })
-export class TituloComponent implements OnInit{
+export class TituloComponent implements OnInit, OnChanges{
 
-  nome:string = 'Agryo'
+  @Input() nome:string = ''
 
-  constructor(){ }
+  constructor(){
+    console.log(`Costructor ${this.nome}`)
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(`OnChanges ${this.nome}`)
+  }
 
   ngOnInit(): void {
+    this.nome = this.nome + " x"
+    console.log(`OnInit ${this.nome}`)
     this.nome = `Olá, ${this.nome}, esse é o Novo Componente.`
   }
 

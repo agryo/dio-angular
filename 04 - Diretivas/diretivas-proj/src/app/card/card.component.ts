6e6c83a34e30f3de1ab002135f32f88e3145d038
Agryo Araujo <agryostallion@gmail.com>
2023-05-23 22:09:00 -0300

    Finalizado Diretivas

--- 04 - Diretivas/diretivas-proj/src/app/card/card.component.ts
+++ 04 - Diretivas/diretivas-proj/src/app/card/card.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
 })
-export class CardComponent {
+export class CardComponent implements OnInit {
+  produtos:string[] = []
+  menuType:string = "diretor"
+
+  constructor(){
+    this.produtos = [
+      "Mouse",
+      "Teclado",
+      "Cabo de Rede",
+      "Fonte de Alimentação",
+      "Monitor"
+    ]
+  }
+
+  ngOnInit(): void {
+  }
+
+  adicionar(){
+    this.produtos.pop()
+  }
+
+  remover(index: number){
+    this.produtos.splice(index, 1)
+  }
 
 }

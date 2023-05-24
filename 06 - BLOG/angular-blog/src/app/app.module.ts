ce13d2bc824f90a05ddf1da070a7698a3c9a4795
Agryo Araujo <agryostallion@gmail.com>
2023-05-24 00:49:59 -0300

    Iniciando o projeto

--- 06 - BLOG/angular-blog/src/app/app.module.ts
+++ 06 - BLOG/angular-blog/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BarraMenuComponent } from './componentes/barra-menu/barra-menu.component';
+import { TituloPrincipalComponent } from './componentes/titulo-principal/titulo-principal.component';
+import { BigCardComponent } from './componentes/big-card/big-card.component';
+import { SmallCardComponent } from './componentes/small-card/small-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarraMenuComponent,
+    TituloPrincipalComponent,
+    BigCardComponent,
+    SmallCardComponent
   ],
   imports: [
     BrowserModule,

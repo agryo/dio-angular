feb2c24c0e46a1a2b1d62f368bdf7f1476160c47
Agryo Araujo <agryostallion@gmail.com>
2023-05-25 18:58:10 -0300

    Básico sobre responsivo de tela.

--- 06 - BLOG/angular-blog/src/app/componentes/titulo-principal/titulo-principal.component.ts
+++ 06 - BLOG/angular-blog/src/app/componentes/titulo-principal/titulo-principal.component.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-titulo-principal',
   templateUrl: './titulo-principal.component.html',
-  styleUrls: ['./titulo-principal.component.css']
+  styleUrls: [
+    './titulo-principal.component.css',
+    './titulo-principal.responsive.component.css'
+  ]
 })
 export class TituloPrincipalComponent {
 
